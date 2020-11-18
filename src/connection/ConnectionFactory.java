7fa7a83885b0a351bbf121dea31ce967a4241505
camil <camil@LAPTOP-EELSCDK7>
2020-11-18 19:16:56 -0300

    Pastas

--- src/connection/ConnectionFactory.java
+++ src/connection/ConnectionFactory.java
@@ -8,9 +8,9 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
    
-	private static final String DRIVER = "con.mysql.jdbc.Driver";
-	private static final String URL = "jdbc: mysql://localhost:3306/Locadora";
-	private static final String USER = "root";
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	private static final String URL = "jdbc: mysql://localhost:3306/locadora";
+	private static final String USER = "";
 	private static final String PASS = "";
 	
 	public static Connection getConnection() {
